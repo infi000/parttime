7130ac23881e10e65a671fcde44c27a5d55cd251
infiooo <113079767@qq.com>
2018-03-06 18:38:38 +0800

    work

--- project24/art-store/include/art-config.inc.php
+++ project24/art-store/include/art-config.inc.php
@@ -1,5 +1,24 @@
 <?php
 define('DBCONNECTION', 'mysql:host=localhost;dbname=art');
-define('DBUSER', 'wb2s18user');
-define('DBPASS', 'wb2s18pass');
+define('DBUSER', 'root');
+define('DBPASS', 'root');
+
+
+
+try {
+    $conn = new PDO(DBCONNECTION, DBUSER, DBPASS);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $stmt = $conn->prepare("SELECT *  FROM paintings LIMIT 0 , 30"); 
+    $stmt->execute();
+     $result = $stmt->setFetchMode(PDO::FETCH_ASSOC); 
+     $res=$stmt->fetchAll();
+     // echo  $stmt;
+   print_r (each($res));
+}
+catch(PDOException $e)
+{
+    echo $e->getMessage();
+}
+
+$conn=null
 ?>
